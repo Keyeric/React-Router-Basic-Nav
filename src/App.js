ae1806859a0a53b8a0fd141b919b298de4f871d4
Keyeric <keyeric.bristol@gmail.com>
2020-01-29 10:17:38 -0700

    Started working in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
+    <Home />
+    <About />
+    <Contact />
     <Navigation />
   </div>
 );
